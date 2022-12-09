8900f91bf0db507e89683136e586db332483da8a
Amit Bhandari <115782156+AmitBhhandari@users.noreply.github.com>
2022-12-09 13:51:59 +0530

    useEffect()

---
+++ src/src/Component/Meals/AvailableMeals.js
@@ -0,0 +1,49 @@
+import MealItem from "./MealItem/MeaIItem";
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+const DUMMY_MEALS = [
+  {
+    id: "m1",
+    name: "Gajer ka Halwa",
+    description: "Sweetest Dish",
+    price: 150,
+  },
+  {
+    id: "m2",
+    name: "Rajma Chawal",
+    description: "A Indian specialty!",
+    price: 100,
+  },
+  {
+    id: "m3",
+    name: "Chole Bhature",
+    description: "Delhi Famous",
+    price:80,
+  },
+  {
+    id: "m4",
+    name: "Kadai Paneer",
+    description: "Protein",
+    price: 350,
+  },
+];
+
+const AvailableMMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+export default AvailableMMeals;
---
+++ src/src/Component/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMMeals from "./AvailableMeals";
+import { Fragment } from "react";
+
+const Meals=()=>{
+return <Fragment>
+    <MealsSummary/>
+    <AvailableMMeals/>
+</Fragment>
+}
+export default Meals;
